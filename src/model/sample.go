8befe3ef1a07217bfe915a03ff21823b3fe8d3dd
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-05-22 15:22:51 -0300

    standandize Sample and Dataset types

--- src/model/sample.go
+++ src/model/sample.go
@@ -1,6 +1,10 @@
 package model
 
-type Sample[T comparable] struct {
-	Label  T
-	Values []float64
+import "github.com/alan-b-lima/handwritten_digits_example/src/nnmath"
+
+type Sample struct {
+	Label  nnmath.Vector
+	Values nnmath.Vector
 }
+
+type Dataset = []Sample
