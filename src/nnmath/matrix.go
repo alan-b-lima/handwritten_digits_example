b012a9d1090a3e904eded2c258b7cd605b5fd24f
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-05-22 15:20:23 -0300

    rework Matrix type

--- src/nnmath/matrix.go
+++ src/nnmath/matrix.go
@@ -1,8 +1,8 @@
 package nnmath
 
 import (
-	"errors"
 	"fmt"
+	"math/rand"
 )
 
 type Matrix struct {
@@ -13,148 +13,116 @@ type Matrix struct {
 
 type Vector = Matrix
 
-func NewMatrix(rows, cols int) (*Matrix, error) {
-
-	if rows <= 0 || cols <= 0 {
-		return nil, errors.New("nnmath: rows and columns must be positive integers")
-	}
-
-	return &Matrix{
+func NewMatrix(rows, cols int) Matrix {
+	return Matrix{
 		Rows: rows,
 		Cols: cols,
 		Data: make([]float64, rows*cols),
-	}, nil
+	}
 }
 
-func NewMatrixInit(rows, cols int, matrix []float64) (*Matrix, error) {
-
-	if rows > 0 || cols > 0 {
-		return nil, errors.New("nnmath: rows and columns must be positive integers")
+func NewMatrixData(rows, cols int, data []float64) Matrix {
+	if len(data) != rows*cols {
+		panic(fmt.Sprintf("data length %d does not match matrix size %d,%d", len(data), rows, cols))
 	}
 
-	size := rows * cols
-
-	if size != len(matrix) {
-		return nil, errors.New("nnmath: the input matrix dimensions do not match the input size")
-	}
-
-	return &Matrix{
+	return Matrix{
 		Rows: rows,
 		Cols: cols,
-		Data: make([]float64, rows*cols),
-	}, nil
-}
-
-func NewIdMatrix(dim int) (*Matrix, error) {
-
-	if dim <= 0 {
-		return nil, errors.New("nnmath: the dimension must be a positive integer")
-	}
-
-	data := make([]float64, dim*dim)
-	for i := range dim {
-		data[i*dim+i] = 1
-	}
-
-	return &Matrix{
-		Rows: dim,
-		Cols: dim,
 		Data: data,
-	}, nil
+	}
 }
 
-func (M *Matrix) Get(row, col int) float64 {
+func NewMatrixRandom(rows, cols int) Matrix {
+	M := NewMatrix(rows, cols)
 
-	if row < 0 || M.Rows <= row || col < 0 || M.Cols <= col {
-		panic(fmt.Sprintf("nnmath: index out of bounds [%d][%d] with dimensions %d, %d", row, col, M.Rows, M.Cols))
+	for i := range rows*cols {
+		M.Data[i] = rand.NormFloat64()
+	}
+
+	return M
+}
+
+func NewVector(size int) Vector {
+	return NewMatrix(size, 1)
+}
+
+func NewVectorData(size int, data []float64) Vector {
+	if len(data) != size {
+		panic(fmt.Sprintf("data length %d does not match vector size %d", len(data), size))
+	}
+
+	return NewMatrixData(size, 1, data)
+}
+
+func NewVectorRandom(size int) Vector {
+	V := NewVector(size)
+
+	for i := range size {
+		V.Data[i] = rand.NormFloat64()
+	}
+
+	return V
+}
+
+func (M Matrix) Get(row, col int) float64 {
+	if row < 0 || row >= M.Rows || col < 0 || col >= M.Cols {
+		panic(fmt.Sprintf("index out of range [%d][%d] with length %d,%d", row, col, M.Rows, M.Cols))
 	}
 
 	return M.Data[row*M.Cols+col]
 }
 
-func (M *Matrix) Set(row, col int, value float64) {
-
-	if row < 0 || M.Rows <= row || col < 0 || M.Cols <= col {
-		panic(fmt.Sprintf("nnmath: index out of bounds [%d][%d] with dimensions %d, %d", row, col, M.Rows, M.Cols))
+func (M Matrix) Set(row, col int, value float64) {
+	if row < 0 || row >= M.Rows || col < 0 || col >= M.Cols {
+		panic(fmt.Sprintf("index out of range [%d][%d] with length %d,%d", row, col, M.Rows, M.Cols))
 	}
 
 	M.Data[row*M.Cols+col] = value
 }
 
-func Add(A, B *Matrix) *Matrix {
-
+func Add(A, B Matrix) Matrix {
 	if A.Rows != B.Rows || A.Cols != B.Cols {
-		panic("nnmath: dimensions do not match for addition")
+		panic("matrix dimensions do not match")
 	}
 
-	R, err := NewMatrix(A.Rows, A.Cols)
-	if err != nil {
-		panic(err)
-	}
+	C := NewMatrix(A.Rows, A.Cols)
 
 	for i := range A.Rows {
 		for j := range A.Cols {
-			R.Set(i, j, A.Get(i, j)+B.Get(i, j))
+			C.Set(i, j, A.Get(i, j)+B.Get(i, j))
 		}
 	}
 
-	return R
+	return C
 }
 
-func Sub(A, B *Matrix) *Matrix {
-
-	if A.Rows != B.Rows || A.Cols != B.Cols {
-		panic("nnmath: dimensions do not match for subtration")
-	}
-
-	R, err := NewMatrix(A.Rows, A.Cols)
-	if err != nil {
-		panic(err)
-	}
-
-	for i := range A.Rows {
-		for j := range A.Cols {
-			R.Set(i, j, A.Get(i, j)-B.Get(i, j))
-		}
-	}
-
-	return R
-}
-
-func SMul(A *Matrix, s float64) *Matrix {
-
-	R, err := NewMatrix(A.Rows, A.Cols)
-	if err != nil {
-		panic(err)
-	}
-
-	for i := range A.Rows {
-		for j := range A.Cols {
-			R.Set(i, j, s*A.Get(i, j))
-		}
-	}
-
-	return R
-}
-
-func Mul(A, B *Matrix) *Matrix {
-
+func Mul(A, B Matrix) Matrix {
 	if A.Cols != B.Rows {
-		panic("nnmath: dimensions do not match for multiplication")
+		panic("matrix dimensions do not match")
 	}
 
-	R, err := NewMatrix(A.Rows, B.Cols)
-	if err != nil {
-		panic(err)
-	}
+	C := NewMatrix(A.Rows, B.Cols)
 
 	for i := range A.Rows {
-		for j := range A.Cols {
+		for j := range B.Cols {
 			var sum float64
 			for k := range A.Cols {
 				sum += A.Get(i, k) * B.Get(k, j)
 			}
-			R.Set(i, j, sum)
+			C.Set(i, j, sum)
+		}
+	}
+
+	return C
+}
+
+func Apply(A Matrix, fn func(float64) float64) Matrix {
+	R := NewMatrix(A.Rows, A.Cols)
+
+	for i := range A.Rows {
+		for j := range A.Cols {
+			R.Set(i, j, fn(A.Get(i, j)))
 		}
 	}
 
